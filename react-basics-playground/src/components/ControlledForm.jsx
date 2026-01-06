14b405b4220601511d00510dc7409fb21997852e
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-06 15:18:59 +0530

    Add react basics playground project

--- react-basics-playground/src/components/ControlledForm.jsx
+++ react-basics-playground/src/components/ControlledForm.jsx
@@ -7,7 +7,7 @@ const ControlledForm = () => {
     <input
       value={name}
       onChange={e => setName(e.target.value)}
-      placeholder="Enter name"
+      placeholder="Enter name mukul"
     />
   );
 };
