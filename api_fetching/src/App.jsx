3c4fab680b96b3f0a3e041588f6ee4b35f8f5202
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-14 18:28:22 +0530

    changes

--- api_fetching/src/App.jsx
+++ api_fetching/src/App.jsx
@@ -1,37 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect ,useState} from 'react'
 import axios from "axios"
+
 const App = () => {
-  const [data , setData] = useState([]);
-  const[loading, setLoading]= useState(true);
+  const [data, setData]=useState([]);
   const[error,setError]=useState(null);
+  const[loading,setLoading]=useState(true);
 
   useEffect(()=>{
 
-    axios.get("https://fakestoreapi.com/products")
-    .then((responce)=>{
-      setData(responce.data);
-      setLoading(false);
-    })
-    .catch((error)=>{
-      setError("Failed to load");
-      setLoading(false);
-    });
+    const fetching = async ()=>{
+      try{
+        const response= await axios.get("https://fakestoreapi.com/products");
+        setData(response.data);
+        
+      }
+      catch(error){
+       setError("Failed to load");
+
+      }
+      finally {
+        setLoading(false);
+      }
+    };
+      fetching();
+
   },[]);
-
-  if(loading)return <h2>Loading...</h2>
-  if(error)return <h2>{error}</h2>
-
-
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>{error}</h2>;
   return (
-   <>
-   {data.map((item)=>(
-    <div key={item.id}>
-        <img src={item.image} alt={item.title} width="100" height="100" />
+    <>
+    {data.map((item)=>(
+      <div key={item.id}>
+         <img src={item.image} alt={item.title} width="100" height="100" />
           <h4>{item.title}</h4>
           <p>₹ {item.price}</p>
-    </div>
-   ))}
-   </>
+      </div>
+    ))}
+    </>
   )
 }
 
