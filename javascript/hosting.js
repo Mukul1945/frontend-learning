1088f83ceb26fa246ed6d2e0a83742369d655aaa
Mukul1945 <mukulchauhan1256@gmail.com>
2026-01-18 16:36:04 +0530

    hosting

---
+++ javascript/hosting.js
@@ -0,0 +1,17 @@
+
+//var
+
+console.log(a);
+var a=10;
+
+//function are fullu hosting
+sayheelo();
+function sayheelo(){
+    console.log("mukul")
+}
+
+//let abd const
+
+console.log(b);
+let b = 20;
+
